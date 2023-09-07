7e0eeef689ea62b9d4ec8f60780fecee6e321a92
VikVanDik <95vlc17@gmail.com>
2023-09-07 15:55:50 +0200

    [add] verified number insertion

--- script/script.js
+++ script/script.js
@@ -1,5 +1,25 @@
 // 1. Creare prompt(parseint) per ottenere dati dall'utente
-// 2. Creare calcolo in base ai km percorsi
-// 3. Applicare sconto al risultato del calcolo
-// 4. Creare l'output del risultato finale
+// 2. Verificare che i dati inseriti siano corretti
+// 3. Creare calcolo in base ai km percorsi
+// 4. Applicare sconto al risultato del calcolo
+// 5. Creare l'output del risultato finale
 
+
+const age = parseInt (prompt("Ciao, benvenuto nella nostra biglietteria, abbiamo bisogo di farti alcune domande per farti conoscere il prezzo del biglietto che devi comprare, quindi, quanti anni hai?"))
+
+const km = parseInt (prompt("Quanti km hai intenzione di percorrere con noi?"))
+
+let message;
+
+
+if (isNaN(age && km)) {
+  message = "Inserire un numero per favore"
+}
+
+
+
+
+
+console.log(age);
+console.log(km)
+console.log(message);
