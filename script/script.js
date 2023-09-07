b9b026ddfca118daea585a849c802759c484b5fe
VikVanDik <95vlc17@gmail.com>
2023-09-07 15:25:54 +0200

    [add] html and js

---
+++ script/script.js
@@ -0,0 +1,5 @@
+// 1. Creare prompt(parseint) per ottenere dati dall'utente
+// 2. Creare calcolo in base ai km percorsi
+// 3. Applicare sconto al risultato del calcolo
+// 4. Creare l'output del risultato finale
+
